232f9b0be8d4fab11924547ca195e80bf8051f29
timjolson <timjolson@user.noreplay.github.com>
2019-07-11 17:16:03 -0500

    Update setup.py to include generalutils

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     packages = find_packages(),
     install_requires = ['PyQt5'],
     dependency_links = [
+        'https://github.com/timjolson/generalutils.git',
         'https://github.com/timjolson/qt_utils.git',
     ],
     tests_require = ['pytest', 'pytest-qt']
